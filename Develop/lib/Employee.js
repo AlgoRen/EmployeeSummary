489ef4c46a8bd887882cd24f67e4f45e22614e51
Ren <algorenstudio@gmail.com>
2020-06-03 17:25:29 -0400

    Created Employee Class, added .gitignore for node modules, and added some comments.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Employee class
+// Create a class of Employee and create a constructor that will build an object.
+// Use object to hold data from inquirer prompt instances.
+
+class Employee {
+    // Employee Constructor with name, id, and email.
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // Employee Methods
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return 'Employee';
+    }
+}
+
+var dude = new Employee('Bob', 10, 'dudemyemail@mail.com')
+console.log(dude.getEmail());
+console.log(dude.getRole());
